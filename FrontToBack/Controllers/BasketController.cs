66ec16d683b8513f7088f27f6c92c3f97625c83b
dbBurzum <khalidsr@code.edu.az>
2022-06-30 17:33:56 +0400

    half done

--- FrontToBack/Controllers/BasketController.cs
+++ FrontToBack/Controllers/BasketController.cs
@@ -1,27 +1,88 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using FrontToBack.Models;
+using FrontToBack.ViewModels;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace FrontToBack.Controllers
 {
     public class BasketController : Controller
     {
-        public IActionResult Index()
+
+        private readonly AppDbContext _context;
+
+        public BasketController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index() 
         {
             return View();
         }
 
-        public IActionResult AddItem()
+        public async Task<IActionResult> AddItem(int? id)
         {
+            if (id==null)return NotFound();
+            Product dbProduct = await _context.Products.FindAsync(id);
+            if (dbProduct == null) return NotFound();
 
-            HttpContext.Session.SetString("name","Khalid");
-
-            return Content("");
+            List<BasketVM> products;
+            if (Request.Cookies["basket"]==null)
+            {
+               products = new List<BasketVM>();
+            }
+            else
+            {
+                products= JsonConvert.DeserializeObject<List<BasketVM>>(Request.Cookies["basket"]);
+            }
+            BasketVM IsExist = products.Find(x => x.Id == id);
+            if (IsExist==null)
+            {
+                BasketVM basketvm = new BasketVM
+                {
+                    Id = dbProduct.Id,
+                    ProductCount = 1
+                };
+                products.Add(basketvm);
+            }
+            else
+            {
+                IsExist.ProductCount++;
+            }
+            
+            Response.Cookies.Append("basket", JsonConvert.SerializeObject(products), new CookieOptions { MaxAge = TimeSpan.FromMinutes(15) });
+            //HttpContext.Session.SetString("name","Khalid");
+            //Response.Cookies.Append("Nick", "Dante", new CookieOptions { MaxAge = TimeSpan.FromDays(365) });
+            return RedirectToAction("showitem", "basket");
         }
         public IActionResult ShowItem()
         {
 
-            string name = HttpContext.Session.GetString("name");
-            return Content(name);
+            //string name = HttpContext.Session.GetString("name");
+            string basket = Request.Cookies["basket"];
+            List<BasketVM> products;
+
+            if (basket != null)
+            {
+                products = JsonConvert.DeserializeObject<List<BasketVM>>(basket);
+                foreach (var item in products)
+                {
+                    Product dbProducts = _context.Products.FirstOrDefault(x => x.Id == item.Id);
+                    item.Name=dbProducts.Name;
+                    item.Price = dbProducts.Price;
+                    item.ImageUrl = dbProducts.ImageUrl;
+                }
+            }
+            else
+            {
+                products = new List<BasketVM>();
+            }
+            return View(products);
         }
     }
 }
--- FrontToBack/ViewComponents/HeaderViewComponent.cs
+++ FrontToBack/ViewComponents/HeaderViewComponent.cs
@@ -1,5 +1,8 @@
 ﻿using FrontToBack.Models;
+using FrontToBack.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -16,6 +19,26 @@ namespace FrontToBack.ViewComponents
 
         public async Task<IViewComponentResult> InvokeAsync()
         {
+            ViewBag.BasketCount = 0;
+            ViewBag.TotalPrice = 0;
+            int TotalCount = 0;
+            double TotalPrice = 0;
+            string basket = Request.Cookies["basket"];
+            if (basket!=null)
+            {
+                List<BasketVM> products = JsonConvert.DeserializeObject<List<BasketVM>>(basket);
+                foreach (var item in products)
+                {
+                    TotalCount += item.ProductCount;
+                }
+                foreach (var item in products)
+                {
+                    TotalPrice += item.Price * item.ProductCount;
+                }
+            }
+            ViewBag.BasketCount = TotalCount;
+            ViewBag.TotalPrice = TotalPrice;
+
             Bio bio = _context.Bios.FirstOrDefault();
             return View(await Task.FromResult(bio));
         }
