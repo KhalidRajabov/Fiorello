343c185bc73407d965b5631ada461cc2b7e309b0
dbBurzum <khalidsr@code.edu.az>
2022-07-05 13:02:20 +0400

    iformfile

--- FrontToBack/Models/Product.cs
+++ FrontToBack/Models/Product.cs
@@ -1,10 +1,15 @@
-﻿namespace FrontToBack.Models
+﻿using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FrontToBack.Models
 {
     public class Product
     {
         public int Id { get; set; }
         public string Name { get; set; }
         public string ImageUrl { get; set; }
+        [NotMapped]
+        public IFormFile Photo { get; set; }
         public double Price { get; set; }
         public int CategoryId { get; set; }
         public Category Category { get; set; }
